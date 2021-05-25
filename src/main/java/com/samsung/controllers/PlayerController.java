75e660a777026f8a10632f385c6ccafc78052b13
Dmitry Petrov <petrovdmitrys@yandex.ru>
2021-05-26 00:06:41 +0500

    Added worker classes. Create user to db works! Get top list workers too!

--- src/main/java/com/samsung/controllers/PlayerController.java
+++ src/main/java/com/samsung/controllers/PlayerController.java
@@ -2,8 +2,6 @@ package com.samsung.controllers;
 
 
 import com.samsung.repository.PlayerRepository;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,25 +12,14 @@ public class PlayerController {
     @Autowired
     private PlayerRepository player;
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "text/plain")
-    public int createPerson(@RequestBody String param){
-        String name = null;
-        String won_rounds = null;
-        String lost_rounds = null;
-        try{
-            JSONObject json = new JSONObject(param);
-            name = json.getString("name_winner");
-            won_rounds = json.getString("won_rounds");
-            lost_rounds = json.getString("lost_rounds");
-
-        }catch(JSONException e){
-            e.getLocalizedMessage();
-            return 0;
-        }
-        System.out.println(name);
-        System.out.println(won_rounds);
-        System.out.println(lost_rounds);
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public int createPerson(@RequestParam String name, String won_rounds, String lost_rounds){
         return player.createPlayer(name, won_rounds, lost_rounds);
+    }
+
+    @RequestMapping(value = "/get-top", method = RequestMethod.GET)
+    public String getInt(){
+        return player.getTop().toString();
     }
 
 
--- src/main/java/com/samsung/repository/PlayerRepository.java
+++ src/main/java/com/samsung/repository/PlayerRepository.java
@@ -1,8 +1,17 @@
 package com.samsung.repository;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
 
 @Component
 public class PlayerRepository {
@@ -11,6 +20,28 @@ public class PlayerRepository {
     private JdbcTemplate jdbcTemplate;
 
     public int createPlayer(String name, String wonRound, String lostRound){
-        return jdbcTemplate.update("INSERT INTO \"Rating\" (\"name_winner\", \"won_rounds\", \"lost_rounds\") VALUES(?, ?, ?)", name, wonRound, lostRound);
+        return jdbcTemplate.update("INSERT INTO \"RATING\" (\"PARTICIPANT\", \"WON_ROUNDS\", \"LOST_ROUNDS\") VALUES(?, ?, ?)", name, wonRound, lostRound);
+    }
+
+    public JSONArray getTop(){
+        JSONObject json;
+        JSONArray jsonArr = new JSONArray();
+        try {
+            Connection conn= Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection();
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM public.\"RATING\"";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                json = new JSONObject();
+                json.put("name", rs.getString("PARTICIPANT"));
+                json.put("wonRounds", rs.getString("WON_ROUNDS"));
+                json.put("lostRounds", rs.getString("LOST_ROUNDS"));
+                jsonArr.put(json);
+            }
+        } catch (SQLException | JSONException e) {
+            e.getLocalizedMessage();
+            return null;
+        }
+        return jsonArr;
     }
 }
